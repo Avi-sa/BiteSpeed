9e8db93019005989d6610875e43881bae2500578
Avi-sa <avivish000@gmail.com>
2023-07-19 01:35:46 +0530

    Identifier changes

--- customers/models.py
+++ customers/models.py
@@ -1,3 +1,47 @@
+import re
+from xml.dom import ValidationErr
 from django.db import models
 
+
 # Create your models here.
+
+
+def mobile_validator(value):
+    # regex for validating a generic international phone number
+    regex = r'^(\+|00){0,2}(9[976]\d|8[987530]\d|6[987]\d|5[90]\d|42\d|3[875]\d|2[98654321]\d|9[8543210]|8[6421]|6[6543210]|5[87654321]|4[987654310]|3[9643210]|2[70]|7|1)\d{1,14}$'
+    rule = re.compile(regex)
+    if not rule.search(value):
+        msg = "Invalid mobile number."
+        raise ValidationErr(msg)
+
+
+def email_validator(value):
+    regex = ''
+    rule = re.compile(regex)
+    if not rule.search(value):
+        msg = "Invalid email address."
+        raise ValidationError(msg)
+
+
+precidence_choices = (
+    ("primary", "primary"),
+    ("secondary", "secondary")
+)
+
+
+class Contacts(models.Model):
+    id = models.AutoField(primary_key=True)
+    email = models.EmailField(max_length=255, null=True, blank=True,
+                              db_index=True, validators=[email_validator])
+    mobile = models.CharField(max_length=32, null=True, blank=True,
+                              db_index=True, validators=[mobile_validator])
+    linkedId = models.ForeignKey(
+        to="self", on_delete=models.CASCADE, null=True, blank=True)
+    linkPrecidence = models.CharField(
+        max_length=40, choices=precidence_choices)
+    created_on = models.DateTimeField(auto_now_add=True)
+    modified_on = models.DateTimeField(auto_now=True)
+    deleted_on = models.DateTimeField(auto_now=False, blank=True, null=True)
+
+    def __str__(self) -> str:
+        return super().__str__()
--- customers/views.py
+++ customers/views.py
@@ -1,3 +1,84 @@
-from django.shortcuts import render
+from itertools import count
+from sys import flags
+from rest_framework.response import Response
+from customers.models import Contacts
+from rest_framework import generics
+from django.db.models import Q
 
 # Create your views here.
+
+
+class IdentifyAPIView(generics.GenericAPIView):
+    """For tracking campaign conversions"""
+
+    http_method_names = ["post"]
+
+    def post(self, request):
+
+        try:
+            email = request.data.get("email", None)
+            phoneNumber = request.data.get("phoneNumber", None)
+        except Exception:
+            return Response({"msg": "phone number or email not found!"}, status=500)
+
+        if not email and not phoneNumber:
+            return Response({"msg": "please pass the valid request args!"}, status=500)
+
+        else:
+            contacts = Contacts.objects.filter(
+                Q(email=email) | Q(mobile=phoneNumber)
+            )
+
+            print('contacts - ', contacts.values())
+
+            primaryUsers = contacts.filter(
+                linkPrecidence="primary").values_list("id", flat=True)
+
+            print('contacts - ', primaryUsers)
+
+            if not contacts.count():
+                Contacts.objects.create(
+                    email=email,
+                    mobile=phoneNumber,
+                    linkedId=None,
+                    linkPrecidence="primary"
+                )
+                print(1)
+
+            elif contacts.count() > 1 and len(primaryUsers) == contacts.count():
+                oldest_id = contacts.order_by("-created_at").first().id
+                contacts.filter(~Q(id__in=oldest_id)).update(linkPrecidence="secondary",
+                                                             linkedId=oldest_id)
+
+                print(2)
+
+            elif primaryUsers and not contacts.filter(email=email, mobile=phoneNumber).count():
+                Contacts.objects.create(
+                    email=email,
+                    mobile=phoneNumber,
+                    linkedId_id=primaryUsers.first(),
+                    linkPrecidence="secondary"
+                )
+
+                print(3)
+
+            contacts = Contacts.objects.filter(
+                Q(email=email) | Q(mobile=phoneNumber)
+            )
+
+            primaryUsers = contacts.filter(
+                linkPrecidence="primary").values_list("id", flat=True)
+
+            secondaryUsers = contacts.filter(
+                ~Q(id__in=primaryUsers)).values_list("id", flat=True)
+
+            response = {
+                "contact": {
+                    "primaryContatctId": primaryUsers.first() if primaryUsers.count() else 0,
+                    "secondaryContactIds": secondaryUsers,
+                    "emails": set(contacts.values_list("email", flat=True)),
+                    "phoneNumbers": set(contacts.values_list("mobile", flat=True))
+                }
+            }
+
+            return Response(response, status=200)
